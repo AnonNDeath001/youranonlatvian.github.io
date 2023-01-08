c13090c521c7e041a77795a7247f6237ac11f66e
AnonNDeath001 <115405576+AnonNDeath001@users.noreply.github.com>
2023-01-08 11:28:24 +0300

    Add files via upload

---
+++ tutorials.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Anonymous | Tutorials</title>
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body>
+<body bgcolor=”#800000">
+<p>&nbsp;</p>
+<p></p>
+<p style="color: white; font-size: 45px; text-align: center;"><strong>Tutorials<br /></strong></p>
+<table style="color: white; border-collapse: collapse; width: 38.7304%; height: 38px; margin-left: auto; margin-right: auto;" border="1">
+<tbody>
+<tr>
+<td style="width: 100%; text-align: center; font-size: 25px;"><strong><span style="color: #ffffff;"><a style="color: #ffffff;" href="/home.php">Home</a></span> | OP | Leaks | Tutorials | About US</strong></td>
+</tr>
+</tbody>
+</table>
+<p style="text-align: center;">&nbsp;</p>
+<table style="background-color: #ffffe0; border-collapse: collapse; width: 68.2204%; margin-left: auto; margin-right: auto;" border="1">
+<tbody>
+<tr>
+<td style="font-size: 18px; width: 22.1935%;">
+<p>Tutorials |</p>
+<ul>
+<li>AnonOps IRC</li>
+<li>Anonymous messengers</li>
+</ul>
+</td>
+<td style="font-size: 25px; width: 42.1778%;">Page "Tutorials". On this page you can learn the basics of anonymity on the Internet. What tools to use for anonymity, hacking, DDoS-Attack, bruteforcing, and more...</td>
+</tr>
+</tbody>
+</table>
+<table style="background-color: #ffffe0; border-collapse: collapse; width: 68.2203%; margin-left: auto; margin-right: auto;" border="1">
+<tbody>
+<tr>
+<td style="width: 100%; text-align: center;">
+<p style="font-size: 45px; text-align: center;">AnonOps IRC:</p>
+<p>Connect AnonOps IRC use: <a title="AnonOps IRC" href="https://webchat.anonops.com/" target="_blank" rel="noopener">AnonOps webchat</a>, HexChat, mIRC or Chatzilla.</p>
+<p>Tutorials how to connect with IRC clients:</p>
+<div class="woo-sc-dividerthree">&nbsp;</div>
+<ul>
+<li>
+<h4>Hexchat Tutorial: <a href="https://anonops.com/xchat.html" target="_blank" rel="noopener">Click here</a></h4>
+</li>
+<li>
+<h4>mIRC Tutorial: <a href="https://anonops.com/mirc.html" target="_blank" rel="noopener">Click here</a></h4>
+</li>
+<li>
+<h4>Chatzilla Tutorial: <a href="https://anonops.com/chatzilla.html" target="_blank" rel="noopener">Click here</a></h4>
+</li>
+<li>
+<h4>Mac Tutorials: <a href="https://anonops.com/mac.html" target="_blank" rel="noopener">Click here</a></h4>
+</li>
+</ul>
+<p style="font-size: 45px; text-align: center;">Anonymous messengers:</p>
+<ul>
+<li>
+<h4 style="font-size: 25px; text-align: center;">Matrix: Element <a href="https://element.io/" target="_blank" rel="noopener">Click here</a></h4>
+</li>
+</ul>
+<p style="text-align: center;">Matrix is an open protocol for instant messaging and file sharing with voice and video support.</p>
+<p style="text-align: center;">It is a decentralized client-server protocol with server-to-server messaging.</p>
+<p style="text-align: center;">An API (program interaction interface) in JSON format was also developed.</p>
+<p style="text-align: center;">The protocol provides the ability to exchange messages with other protocols through bridges.</p>
+<p style="text-align: center;">Bridges are programs that work in conjunction with a server and forward messages between different networks. Bridges currently exist for:</p>
+<p style="text-align: center;">1. Telegram</p>
+<p style="text-align: center;">2. Discord</p>
+<p style="text-align: center;">3. Gitter</p>
+<p style="text-align: center;">4. Slack/Mattermost</p>
+<p style="text-align: center;">5. IRC</p>
+<p style="text-align: center;">6. WhatsApp</p>
+<p style="text-align: center;">7. XMPP</p>
+<p style="text-align: center;">8. Facebook Messenger</p>
+<p style="text-align: center;">9. Signal</p>
+<p style="text-align: center;">10. Skype</p>
+<ul style="list-style-type: disc;">
+<li>
+<h4 style="font-size: 25px; text-align: center;">Briar: <a href="https://briarproject.org/" target="_blank" rel="noopener">Click here</a></h4>
+</li>
+</ul>
+<p>Matrix is an open protocol for instant messaging and file sharing with voice and video support.</p>
+<p>It is a decentralized client-server protocol with server-to-server messaging. An API (program interaction interface) in JSON format was also developed.</p>
+<p>The protocol provides the ability to exchange messages with other protocols through bridges.</p>
+<p>Bridges are programs that work in conjunction with a server and forward messages between different networks. Bridges currently exist for:</p>
+<p>- Doesn't support image's</p>
+<p>- Doesn't support GIF</p>
+<p>- Doesn't support Emodji</p>
+<p>- Doesn't support Video\audio calls</p>
+<p>- Doesn't support Voice messages</p>
+<p>&nbsp;</p>
+<ul>
+<li>
+<h4>Chatzilla Tutorial: <a href="https://anonops.com/chatzilla.html" target="_blank" rel="noopener">Click here</a></h4>
+</li>
+<li>
+<h4>Mac Tutorials: <a href="https://anonops.com/mac.html" target="_blank" rel="noopener">Click here</a></h4>
+</li>
+</ul>
+</td>
+</tr>
+</tbody>
+</table>
